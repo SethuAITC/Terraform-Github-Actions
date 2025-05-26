f729fe5c255eef1ed789b1fa4663d5d58d7d80cb
SethuAITC <sethupathyaitc@gmail.com>
2025-05-26 19:32:48 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,13 +33,13 @@ resource "aws_security_group" "instance_sg" {
 
 resource "aws_instance" "web" {
   ami                         = "ami-04f167a56786e4b09"  # Ubuntu 22.04 LTS for us-east-2
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   subnet_id                   = "subnet-07d27fd7bb3e4126b"
   vpc_security_group_ids      = [aws_security_group.instance_sg.id]
   associate_public_ip_address = true
   key_name                    = "ssh-key"  
 
   tags = {
-    Name = "Terraform-EC2"
+    Name = "Terraform-EC2 Latest"
   }
 }
