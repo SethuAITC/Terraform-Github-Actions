7dfde0f528419030524ed783e62a07f097ad1b72
SethuAITC <sethupathyaitc@gmail.com>
2025-05-27 12:20:31 +0530

    Create securitygroup.tf

---
+++ terraform/securitygroup.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "instance_sg" {
+  name        = "instance-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = "vpc-02dd22c6a71b9fbd6"
+
+ingress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+
+  ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  ingress {
+    description      = "SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
