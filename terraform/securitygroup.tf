de5e148ac8d80e493c5c256e881cdfc4fc160604
SethuAITC <sethupathyaitc@gmail.com>
2025-05-27 12:28:24 +0530

    Update securitygroup.tf

--- terraform/securitygroup.tf
+++ terraform/securitygroup.tf
@@ -9,7 +9,7 @@ ingress {
     to_port          = 443
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+  
   }
 
 
@@ -19,7 +19,7 @@ ingress {
     to_port          = 80
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+   
   }
 
   ingress {
@@ -28,7 +28,7 @@ ingress {
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    
   }
 
   egress {
@@ -36,6 +36,6 @@ ingress {
     to_port          = 0
     protocol         = "-1"
     cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+   
   }
 }
