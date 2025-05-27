3b84f522a2eb3129a9eb6c6ce8a8d0036b008d0f
SethuAITC <sethupathyaitc@gmail.com>
2025-05-27 12:29:53 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 resource "aws_instance" "web" {
   ami                         = "ami-04f167a56786e4b09"  # Ubuntu 22.04 LTS for us-east-2
-  instance_type               = "t2.micro"
+  instance_type               = "t2.medium"
   subnet_id                   = "subnet-07d27fd7bb3e4126b"
   vpc_security_group_ids      = [aws_security_group.instance_sg.id]
   associate_public_ip_address = true
